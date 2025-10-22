9225426fdc949f8f37297d80add4c8cb507bf692
Noskina Olha <noskinaolga1@gmail.com>
2025-10-22 19:25:48 +0200

    add users list to search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './search.css';
 import { Search as SearchIcon } from 'lucide-react';
 import { CircleX } from 'lucide-react';
+import { BadgeCheck } from 'lucide-react';
+
 
 
 
 const Search = ({onClose}) => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://picsum.photos/v2/list?page=6&limit=15')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
   return (
     <div
       className='searchContainer'
@@ -18,7 +28,7 @@ const Search = ({onClose}) => {
           <div className='searchInput'>
             <SearchIcon className='searchIcon' />
             <input type='text' name='search' placeholder='Search' />
-            <CircleX className='closeIcon' size={15}/>
+            <CircleX className='closeIcon' size={5}/>
           </div>
         </div>
 
@@ -27,7 +37,22 @@ const Search = ({onClose}) => {
         </div>
 
         <div className='searchResults'>
-          No recent requests.
+          {/*<p className='noResults'>No recent requests.</p>*/}
+          <div>
+            {users.map((user, i) => (
+              <div className='users-search' key={i}>
+                <div className='users-info-search'>
+                  <div key={i.id} className='users-img-search'>
+                    <img src={user.download_url} alt='profile image' className='img-search-users' width={40}/>
+                  </div>
+                  <div className='users-text-search'>
+                    <div key={i.id} className='searchUsersName'>{user.author} <BadgeCheck className='badgeCheck' size={14}/></div>
+                    <div key={i.id} className='searchUsersSub '>{user.author} • Followers: 125K </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
